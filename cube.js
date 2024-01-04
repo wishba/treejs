7d155fd12b40a436c227be4579ffba45b964c44c
wishba <wisnubayu34@gmail.com>
2024-01-04 19:57:24 +0700

    cube and line

--- cube.js
+++ cube.js
@@ -1,21 +1,25 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// cube
+// cube geometry
 const geometry = new THREE.BoxGeometry(1, 1, 1);
+
+// cube material
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+
+// cube is geometry with material
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
-
-// render
+// render with animation
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
